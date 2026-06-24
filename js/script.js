c5b7e2aac2e362e3dc667e7917473c61e03cd2fb
Arthur <chefinh35@gmail.com>
2026-06-24 17:23:29 -0300

    att7

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ async function login() {
 
         // redireciona
         setTimeout(() => {
-    window.location.href = "html/dashboard.html";
+    window.location.href = "./dashboard.html";
 }, 800);
     } catch (err) {
         console.error(err);
